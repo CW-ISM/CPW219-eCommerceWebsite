40e965645267af747efc50237113860ca96a2b26
CW-ISM <cwismgitism@gmail.com>
2025-08-21 20:07:58 -0700

    Added registration action and view for Register

---
+++ eCommerce/Data/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eCommerce.Models;
+
+namespace eCommerce.Data;
+
+public class MemberController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
