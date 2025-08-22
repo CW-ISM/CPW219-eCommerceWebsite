e3b18f6688d77e6d6ae1d4786b865b3de0495170
Chris Wahl <cwismgitism@gmail.com>
2025-08-21 17:53:49 -0700

    Product edit (#21)
    * Added Update button in Index.cshtml
    
    * Added Edit action to `ProductController`
    
    * Add edit action to `ProductController`
    
    Implemented a new HTTP Post method `Edit` in the `ProductController`

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -42,4 +42,35 @@ public class ProductController : Controller
         }
         return View(product);
     }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        Product? product = _context.Products
+            .Where(p => p.Id == id)
+            .FirstOrDefault();
+
+        if (product == null)
+        {
+            //TempData["ErrorMessage"] = "Product not found.";
+            return NotFound();
+        }
+        return View(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Product product)
+    {
+        if (!ModelState.IsValid)
+        {
+            _context.Update(product); // Update the product in the context
+            await _context.SaveChangesAsync(); // Save changes to the database
+
+            TempData["SuccessMessage"] = $"{product.Name} was updated successfully!";
+
+            return RedirectToAction("Index");
+        }
+
+        return View(product);
+    }
 }
